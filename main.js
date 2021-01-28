9fe77874b994b6272a7a5314a9b34050abdfd979
Tip Powell <powelltip@gmail.com>
2021-01-27 22:31:10 -0500

    HTML looks most of the way there, most of the styling is ok but the JS is something i'm woefully lost on

---
+++ main.js
@@ -0,0 +1,33 @@
+const button = document.querySelectorAll(button);
+// const decimal = document.getElementById(“decimal”);
+const clear = document.getElementById(".data-clear”);
+const display = document.querySelector('.output');
+
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    dontHaveSecondOperand: false,
+    operator: null,
+}
+
+function inputClear() {
+    displayValue = "0"
+}
+
+let button = document.querySelector("="(input)
+
+function inputOperator(operator)
+
+function updateDisplay() {
+    const display = document.querySelector('.result');
+    display.value = calculator.display
+}
+
+for (let value of document.querySelectorAll('.button')) {
+    value.
+}
+
+let button = document.querySelector('.cancelButton')
+button.addEventListener('click', function () {
+alert('Are you sure you want to cancel?')
+})
